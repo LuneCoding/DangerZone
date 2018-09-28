f403746528f1d248d4ae8c38436cf738de5e8214
Dennisvdloo <dennis.van.der.loo@hotmail.com>
2018-09-28 13:30:23 +0200

    Added Vive tracker/ Trigger check/ Controller fix

---
+++ Assets/Scripts/Player/TriggerChecker.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TriggerChecker : MonoBehaviour
+{
+    private SteamVR_TrackedObject trackedObject;
+
+    [SerializeField]
+    private bool trigger;
+	// Use this for initialization
+	void Start ()
+    {
+        trackedObject = GetComponent<SteamVR_TrackedObject>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        var device = SteamVR_Controller.Input((int)trackedObject.index);
+
+        trigger = device.GetPress(Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger);
+	}
+}
