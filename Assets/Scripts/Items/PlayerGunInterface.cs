0793b4f47164274026bc1d8c492e98aeb735efc0
Dennisvdloo <dennis.van.der.loo@hotmail.com>
2018-10-10 10:42:14 +0200

    Adding Forge Networking/ Fixing errors/ Switching to Unity 2018.2.10

--- Assets/Scripts/Items/PlayerGunInterface.cs
+++ Assets/Scripts/Items/PlayerGunInterface.cs
@@ -24,7 +24,6 @@ namespace Assets.Scripts.Items
 
         [SerializeField] private int _maxRoundsMag = DEFAULT_MAX_ROUNDS_MAG;
         [SerializeField] private TextMeshProUGUI _lineOfFireWarning;
-        [SerializeField] private ParticleSystem _explosion;
 
         private const int DEFAULT_MAX_ROUNDS_MAG = 15;
 
@@ -76,9 +75,7 @@ namespace Assets.Scripts.Items
             if (_currentRoundsInMag > 0)
             {
                 base.Shoot();
-                HandleSuicide();
                 _currentRoundsInMag--;
-                // Debug.Log("SHOOT");
                 if (ScenarioBase.Instance != null)
                 {
                     if (ScenarioBase.Instance.Started)
@@ -93,7 +90,6 @@ namespace Assets.Scripts.Items
             {
                 // Gun empty
                 AudioController.PlayAudio(gameObject, AudioCategory.GunTrigger);
-               // Debug.Log("CLICK CLICK");
             }
             OnShoot(_currentRoundsInMag <= 0);
         }
@@ -105,55 +101,6 @@ namespace Assets.Scripts.Items
         public void ReloadGun()
         {
             _currentRoundsInMag = _maxRoundsMag;
-            //Debug.Log("RELOADING");
-        }
-
-        /// <summary>
-        /// Checks if the player wants to commit suicide and help them do it.
-        /// </summary>
-        public void HandleSuicide()
-        {
-            if (Player.Player.Instance == null) return;
-            Vector3 barrelDirection = RaycastObject.transform.TransformDirection(Vector3.up);
-            RaycastHit hit;
-            if (!Physics.Raycast(RaycastObject.transform.position, barrelDirection, out hit)) return;
-
-            if (hit.collider.gameObject != Player.Player.Instance.PlayerCameraEye.gameObject) return;
-
-
-            //play explosion if one is added to scene
-            if (_explosion != null)
-            {
-                // Explode
-                _explosion.transform.position = Player.Player.Instance.PlayerCameraEye.transform.position;
-                Instantiate(_explosion);
-                _explosion.Play();
-                AudioController.PlayAudioAtPoint(_explosion.transform.position, AudioCategory.Explosion);
-
-                // Return to menu
-                if (Scenario.ScenarioBase.Instance != null)
-                {
-                    Scenario.ScenarioBase.Instance.Stop();
-
-                    Scenario.ScenarioBase.Instance.BackToMainMenu(_explosion.main.duration - 1);
-                }
-            }
-            else
-            {
-                Debug.LogWarning("Explosion particlesystem is not set!");
-                //just invoke the stopbutton to make sure it wont crash the game
-                Button stop = GameObject.Find("StopBtn").GetComponent<Button>();
-
-                if (stop != null)
-                {
-                    stop.onClick.Invoke();
-                }
-                else
-                {
-                    Debug.LogError("Stopbutton is not named 'StopBtn'.");
-                    throw new Exception("Stopbutton is not named 'StopBtn'.");
-                }
-            }
         }
     }
 }
